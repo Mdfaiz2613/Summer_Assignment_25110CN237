53a8028cd17bc17eae94015eb6d6135cf4b9496c
Mdfaiz2613 <mf016433@gmail.com>
2026-06-05 16:44:58 +0530

    add new program

---
+++ Day3/lcm.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+
+    int maxNum = (a > b) ? a : b;
+
+    while (true) {
+        if (maxNum % a == 0 && maxNum % b == 0) {
+            cout << "LCM = " << maxNum;
+            break;
+        }
+        maxNum++;
+    }
+
+    return 0;
+}
